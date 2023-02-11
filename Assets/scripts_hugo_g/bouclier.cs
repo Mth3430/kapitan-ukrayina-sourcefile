53381973969e6fe229c0e4175481abb08e9b8edd
Hugo-Gardes <hugo.gardes@epitech.eu>
2023-02-11 15:10:47 +0100

    add shield

---
+++ Assets/scripts_hugo_g/bouclier.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouclier : MonoBehaviour
+{
+    public Vector3 Pos_init;
+    public GameObject parent;
+    public float speed = 1f;
+    public float dist = 5f;
+    public short dir = 1;
+    public short dir_init = 1;
+    public float damage = 1f;
+
+    public void set_parent(GameObject parent) {
+        this.parent = parent;
+    }
+
+    public void set_dir(short dir)
+    {
+        this.dir = dir;
+        this.dir_init = dir;
+    }
+
+    void Start()
+    {
+        Debug.Log("bouclier");
+        Pos_init = transform.position;
+    }
+
+    private void OnDestroy() {
+        parent.GetComponent<player_movement>().set_shoot(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        print("collision");
+        if (other.gameObject.GetComponent<ennemy_scripts>()) {
+            other.gameObject.GetComponent<ennemy_scripts>().take_damages(damage);
+        }
+        if (other.gameObject.tag != "Player") {
+            dir *= -1;
+        }
+    }
+
+    void Update()
+    {
+        if (((transform.position.x > Pos_init.x + dist && dir_init == 1) || (transform.position.x < Pos_init.x - dist && dir_init == -1)) && (dir == dir_init))
+        {
+            dir *= -1;
+        }
+        if (dir != dir_init && ((transform.position.x <= Pos_init.x && dir_init == 1) || (transform.position.x >= Pos_init.x && dir_init == -1)))
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector3.right * Time.deltaTime * speed * dir);
+    }
+}
--- Assets/scripts_hugo_g/player_movement.cs
+++ Assets/scripts_hugo_g/player_movement.cs
@@ -7,38 +7,87 @@ public class player_movement : MonoBehaviour
     public string[] playerInput = new string[3] { "z", "q", "d" };
     public float speed = 1f;
     public float jump = 1f;
+    public float atk_range = 1f;
+    public float atk_damage = 1f;
     public float dist_to_ground = 1f;
+    public bool is_shoot = false;
     public LayerMask ground;
+    public LayerMask ennemy;
+    public KeyCode attack_key = KeyCode.E;
+    public GameObject shield_prefab;
+    private short dir = 1;
+
+    public void set_shoot(bool shoot) {
+        is_shoot = shoot;
+    }
 
     bool is_grounded() {
         return Physics2D.Raycast(transform.position, Vector3.down, dist_to_ground, ground).collider != null;
     }
 
-    void LateUpdate()
+    void attack() {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector3.right * dir, atk_range, ennemy);
+        if (hits.Length > 0) {
+            foreach (RaycastHit2D hit in hits) {
+                if (hit.collider.GetComponent<ennemy_scripts>()) {
+                    hit.collider.GetComponent<ennemy_scripts>().take_damages(atk_damage);
+                }
+            }
+        }
+    }
+
+    void shield_atk() {
+        Debug.Log("shield");
+        GameObject obj = Instantiate(shield_prefab, transform.position, Quaternion.identity);
+        obj.GetComponent<bouclier>().set_dir(dir);
+        obj.GetComponent<bouclier>().set_parent(gameObject);
+        is_shoot = true;
+    }
+
+    void FixedUpdate()
     {
-        if (Input.GetKey(playerInput[0]))
+        if (is_shoot) {
+            return;
+        }
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(playerInput[0]))
         {
             if (is_grounded())
             {
-                Debug.Log("jump");
                 transform.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jump);
             }
         }
+    }
+
+    void LateUpdate()
+    {
+        if (is_shoot) {
+            return;
+        }
         if (Input.GetKey(playerInput[1]))
         {
+            if (dir != -1)
+            {
+                dir = -1;
+                transform.localScale *= -1;
+            }
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
         if (Input.GetKey(playerInput[2]))
         {
+            if (dir != 1)
+            {
+                dir = 1;
+                transform.localScale *= -1;
+            }
             transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
-            if (is_grounded())
-            {
-                Debug.Log("jump");
-                transform.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jump);
-            }
+            attack();
+        }
+        if (Input.GetKeyDown(attack_key))
+        {
+            shield_atk();
         }
     }
 }
